b35d1c1bda6251889127a09efab418f5dbbbe950
Sunniva <sunnivaunsgaard@gmail.com>
2021-07-22 21:39:36 +0200

    updated the api
    https

--- src/aurora.js
+++ src/aurora.js
@@ -5,12 +5,11 @@ let location = document.querySelector("#type-location").value;
 let countryCode = document.querySelector("#type-country-code").value;
 let stateCode = document.querySelector("#type-state-code").value;
 extractLocation(location, countryCode, stateCode);
-
 }
 
 function extractLocation(location, countryCode, stateCode) {
 let apiKey = `81bf8dd320f01a5acdd432f8343859e1`;
-let apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${location},${stateCode},${countryCode}&limit=1&appid=${apiKey}`;
+let apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${location},${stateCode},${countryCode}&limit=1&appid=${apiKey}`;
 document.querySelector("#name-of-place").innerHTML = `The probability of seeing the Aurora in ${location.toUpperCase()}: `; 
 findProbability.innerHTML = `We are calculating`;
 
