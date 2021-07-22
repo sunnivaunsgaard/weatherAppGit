ae4be4d981071baba6390709cddd7de22f4f695e
Sunniva <sunnivaunsgaard@gmail.com>
2021-07-22 17:46:27 +0200

    made api work and updated to change innerHTML

--- src/aurora.js
+++ src/aurora.js
@@ -4,15 +4,15 @@ event.preventDefault();
 let location = document.querySelector("#type-location").value;
 let countryCode = document.querySelector("#type-country-code").value;
 let stateCode = document.querySelector("#type-state-code").value;
-extractLocation(location, countryCode, stateCode); 
+extractLocation(location, countryCode, stateCode);
 
 }
 
 function extractLocation(location, countryCode, stateCode) {
 let apiKey = `81bf8dd320f01a5acdd432f8343859e1`;
 let apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${location},${stateCode},${countryCode}&limit=1&appid=${apiKey}`;
+document.querySelector("#name-of-place").innerHTML = `The probability of seeing the Aurora in ${location} is: `; 
 axios.get(apiUrl).then(reportLocation);
-console.log(apiUrl);
 }
 
 function reportLocation (response) {
@@ -20,15 +20,11 @@ let lat = response.data[0].lat;
 let lon = response.data[0].lon;
 let auroraUrl = `https://api.auroras.live/v1/?type=all&lat=${lat}&long=${lon}&forecast`;
 axios.get(auroraUrl).then(searchAurora);
-console.log(auroraUrl);
 }
 
 function searchAurora(response) {
-
-}
-
-function reportAurora() {
-
+let probability = response.data.probability.value;
+document.querySelector("#description-aurora").innerHTML = ` ${probability}%`;
 }
 
 
